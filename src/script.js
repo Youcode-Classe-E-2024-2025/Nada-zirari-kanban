13a62e2a711c88c84789d3669165eee94192785e
Nada Zirari <zirarinada20@gmail.com>
2024-11-01 15:06:14 +0100

    jai modifier la fonction en js pour ajouter une tache

--- src/script.js
+++ src/script.js
@@ -1,13 +1,37 @@
 // Ouvrir le modal
+
 function ouvrirModal() {
-  console.log("ouvrirModal appelé");
-  document.getElementById("modal").classList.remove("hidden");
+document.getElementById("modal").classList.remove("hidden");
 }
+
+// Fermer le modal
 
 function fermerModal() {
   const modal = document.getElementById("modal");
   modal.classList.add("hidden"); // Masquer le modal
   document.getElementById("taskForm").reset(); // Réinitialiser le formulaire
-  console.log("Modal fermé"); // Ajoutez ceci pour voir si la fonction est appelée
+
 }
 
+// Fermer le modal lorsque l'utilisateur clique en dehors
+
+window.onclick = function(event) {
+  const modal = document.getElementById("modal");
+  if (event.target === modal) {
+    fermerModal();
+  }
+};
+
+
+// Ajouter un écouteur d'événements pour le formulaire
+document.getElementById("taskForm").addEventListener("submit", ajouterTache);
+
+// Fonction pour ajouter une nouvelle tâche
+function ajouterTache(event) {
+  event.preventDefault(); // Empêche le rechargement de la page
+
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  const date = document.getElementById("date").value;
+  const status = document.querySelector('input[name="status"]:checked').value;
+  const priority = document.querySelector('input[name="priority"]:checked').value; // Récupérer la priorité
